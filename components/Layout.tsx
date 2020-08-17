917ffb878a3b0e85b55dfc6919d8d2cb05dd92c1
wonderfulboyx <wonderfulboyx@gmail.com>
2020-08-18 05:55:52 +0900

    mdx内でogp画像を設定できるように

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import {Container} from "@theme-ui/components";
 
 export const siteTitle = 'wonderfulboyxのblog'
+export const defaultOgImage: string | null = null
 
 const Layout: React.FC = ({children}) => {
   return (
@@ -12,6 +13,10 @@ const Layout: React.FC = ({children}) => {
         <title>{siteTitle}</title>
         <link rel="icon" href="/favicon.ico"/>
         <meta name="og:title" content={siteTitle}/>
+        {defaultOgImage && <meta
+          property="og:image"
+          content={defaultOgImage}
+        />}
         <meta name="twitter:card" content="summary_large_image"/>
       </Head>
       <Navbar/>
